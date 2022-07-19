14a69bacc782e60785f18e64b719679e8e392de2
Sagar Hapani <hapanisagar007@gmail.com>
2022-07-19 22:14:38 +0100

    my second commit

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -4,7 +4,7 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("My First Application1");
-        System.out.println("Sagar Hapani"); //i m sagar
+        System.out.println("Sagar Hapani B"); //i m sagar
     }
 
 }
