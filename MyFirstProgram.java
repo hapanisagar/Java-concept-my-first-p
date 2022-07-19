d9bc058999509283a788ba347f677a6d5fb3c1e9
Sagar Hapani <hapanisagar007@gmail.com>
2022-07-19 20:55:28 +0100

    my first commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,14 @@
+package Javaconcepts;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("My First Application1");
+        System.out.println("Sagar Hapani");
+    }
+
+}
+
+
+
+
